70d89aa1e33cabe6e77e3ea506808d8b8960b91e
HalaSwed <alid68831@gmail.com>
2026-08-19 22:16:06 +0300

    Add Railway backend configuration

---
+++ backend/config.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once "db.php";
+require_once "logger.php";
+require_once "notifier.php";
+
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
+$secret_key = getenv("SECRET_KEY");
+$gemini_api_key = getenv("GEMINI_API_KEY");
